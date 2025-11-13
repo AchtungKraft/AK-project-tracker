afae41a7de7f03b889edbb152268ff65229cd8d6
Achtung Kraft <sales@achtungkraft.com>
2025-11-13 13:50:37 +0000

    Manual code change

--- src/components/parts/BuildsDashboard.jsx
+++ src/components/parts/BuildsDashboard.jsx
@@ -246,7 +246,7 @@ export default function BuildsDashboard() {
 
                         <Button
                         variant="outline"
-                        size="sm" className="bg-red-600 px-3 text-xs font-medium rounded-md inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border shadow-sm hover:bg-accent hover:text-accent-foreground h-8 w-full border-gray-700 hover:border-red-900/50 gap-2">
+                        size="sm" className="bg-lime-600 px-3 text-xs font-medium rounded-md inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border shadow-sm hover:bg-accent hover:text-accent-foreground h-8 w-full border-gray-700 hover:border-red-900/50 gap-2">
 
 
                           <Eye className="w-4 h-4" />
@@ -259,7 +259,7 @@ export default function BuildsDashboard() {
 
                         <Button
                         variant="outline"
-                        size="sm" className="bg-lime-300 px-3 text-xs font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border shadow-sm hover:bg-accent hover:text-accent-foreground h-8 w-full border-gray-700 hover:border-red-900/50">
+                        size="sm" className="bg-slate-600 text-slate-50 px-3 text-xs font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border shadow-sm hover:bg-accent hover:text-accent-foreground h-8 w-full border-gray-700 hover:border-red-900/50">
 
 
                           Open Build
